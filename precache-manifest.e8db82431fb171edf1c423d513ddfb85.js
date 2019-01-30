c265becc36109b78a0d80ad1aecd5009417ac827
180312 <31240925+180312@users.noreply.github.com>
2019-01-30 18:28:58 +0100

    Updates

---
+++ precache-manifest.e8db82431fb171edf1c423d513ddfb85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f724d0fed5d69e830ea3",
+    "url": "/workshop-experis/static/css/main.888d54fc.chunk.css"
+  },
+  {
+    "revision": "f724d0fed5d69e830ea3",
+    "url": "/workshop-experis/static/js/main.f724d0fe.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/workshop-experis/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "ff764c06767e6377e8df",
+    "url": "/workshop-experis/static/js/runtime~main.ff764c06.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/workshop-experis/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e104edf0140425c41d6bfcfac17d616f",
+    "url": "/workshop-experis/index.html"
+  }
+];
